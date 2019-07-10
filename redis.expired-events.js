fb2305daff4d5e6441b347b5d0e4845855de7cdd
Glitch (hello-express) <none>
2019-07-10 15:44:05 +0000

    ☄️🚙 Checkpoint ./redis.repo.js:1701921/315 ./redis.expired-events.js:1701921/374

--- redis.expired-events.js
+++ redis.expired-events.js
@@ -1,11 +1,24 @@
 //import PubSub from "./pubsub";
 
 const PubSub = require('./pubsub');
+const RedisRepo = require('./redis.repo');
+
+const redisRepo = new RedisRepo();
 
 const RedisExpiredEvents = function(){
   PubSub.subscribe("__keyevent@0__:expired");
   PubSub.on("message", async (channel, message) => {
     // Handle event
+    const [type, key] = message.split(":");
+    switch (type) {
+      case "reminder": {
+        const value = await redisRepo.get(key);
+        console.log("TYPE: ", type);
+        console.log("KEY: ", key);
+        console.log("VALUE: ", value);
+        break;
+        }
+    }
   });
 }
 
--- redis.repo.js
+++ redis.repo.js
@@ -2,13 +2,27 @@ const Redis = require('ioredis');
 
 //connect to RedisLabs
 
-const RedisRepo =  new class RedisRepo {
+class RedisRepo {
   constructor() {
     this.redis = new Redis(process.env.REDIS_CONN,{connectionName:`vaporware-repo`});
     this.redis.on("ready", () => {
       this.redis.config("SET", "notify-keyspace-events", "Ex");
     });
   }
+  
+  get(key) {
+    return this.redis.get(key);
+  }
+  
+  setReminder(key, value, expire) {
+    this.redis
+      .multi()
+      .set(key, value)
+      .set(`reminder:${key}`, 1)
+      .expire(`reminder:${key}`, expire)
+      .exec();
+  }
+  
 }
 
 module.exports = RedisRepo;
