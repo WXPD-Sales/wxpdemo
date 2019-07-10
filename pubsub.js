fa82aa810352c255b7dba10ddcd2889b86426570
Glitch (hello-express) <none>
2019-07-10 16:16:07 +0000

    👠🗽 Checkpoint ./pubsub.js:1701921/58 ./server.js:1701921/1

--- pubsub.js
+++ pubsub.js
@@ -6,7 +6,7 @@ const Redis = require('ioredis');
 const subscriber = new Redis(process.env.REDIS_CONN,{connectionName:`vaporware-sub`}); //connection to publish events & write data
 const publisher = new Redis(process.env.REDIS_CONN,{connectionName:`vaporware-pub`}); // connection to subcribe to events
 
-class PubSub {
+module.exports = new class PubSub {
   publish(channel, message) {
     publisher.publish(channel, message);
   }
@@ -18,6 +18,5 @@ class PubSub {
       callback(channel, message);
     });
   }
-};
+}();
 
-module.exports = { PubSub: PubSub};
