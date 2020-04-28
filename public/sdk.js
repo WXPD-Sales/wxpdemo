16e38709d0db76a33586cf0bd3034bdbe7d88a82
Glitch (hello-express) <none>
2020-04-28 01:59:26 +0000

    🌟🌹 Checkpoint ./public/sdk.js:1701921/1317

--- public/sdk.js
+++ public/sdk.js
@@ -58,47 +58,7 @@ webex.meetings.register().catch(err => {
 function bindMeetingEvents(meeting) {
   
   console.log('This meeting', meeting);
-  
-  //look for devices---
-  meeting.getDevices().then((deviceInfos) => {
-  const values = selectors.map((select) => select.value);
-
-  selectors.forEach((select) => {
-    while (select.firstChild) {
-      select.removeChild(select.firstChild);
-    }
-  });
-
-  for (let i = 0; i !== deviceInfos.length; i += 1) {
-    const deviceInfo = deviceInfos[i];
-    const option = document.createElement('option');
-
-    option.value = deviceInfo.deviceId;
-    if (deviceInfo.kind === 'audioinput') {
-      option.text = deviceInfo.label || `microphone ${audioInputSelect.length + 1}`;
-      audioInputSelect.appendChild(option);
-    }
-    else if (deviceInfo.kind === 'audiooutput') {
-      option.text = deviceInfo.label || `speaker ${audioOutputSelect.length + 1}`;
-      audioOutputSelect.appendChild(option);
-    }
-    else if (deviceInfo.kind === 'videoinput') {
-      option.text = deviceInfo.label || `camera ${videoSelect.length + 1}`;
-      videoSelect.appendChild(option);
-    }
-    else {
-      console.log('Some other kind of source/device: ', deviceInfo);
-    }
-  }
-  selectors.forEach((select, selectorIndex) => {
-    if (Array.prototype.slice.call(select.childNodes).some((n) => n.value === values[selectorIndex])) {
-      select.value = values[selectorIndex];
-    }
-  })
-}).catch(e => console.err(e));
-  
-  //-----
-  
+    
   meeting.on("error", err => {
     console.error("Meeting error -", err);
   });
