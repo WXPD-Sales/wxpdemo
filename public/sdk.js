82501e630af330679cbb7a2915de71ee6afba9d8
Glitch (hello-express) <none>
2020-04-28 02:47:27 +0000

    👨‍👨‍👦🚤 Checkpoint ./public/sdk.js:1701921/2119 ./views/sdksoundcheck.html:1701921/5

--- public/sdk.js
+++ public/sdk.js
@@ -8,15 +8,71 @@ const webex = (window.webex = Webex.init({
 const destination = Cookies.get("target");
 const jwt = Cookies.get("token");
 console.log(`Found JWT - ${jwt}`);
-/*
 
-const webex = window.Webex.init({
-  credentials: {
-    access_token: myAccessToken
+//-----
+//AV Sources
+
+const audioInputSelect = document.querySelector('select#audioSource');
+const audioOutputSelect = document.querySelector('select#audioOutput');
+const videoSelect = document.querySelector('select#videoSource');
+
+const audio = {};
+const video = {};
+const media = {
+  receiveVideo: true,
+  receiveAudio: true,
+  receiveShare: false,
+  sendShare: false,
+  sendVideo: true,
+  sendAudio: true
+};
+
+// setting up the devices
+const selectors = [audioInputSelect, audioOutputSelect, videoSelect];
+
+navigator.mediaDevices.enumerateDevices().then((deviceInfos) => {
+const values = selectors.map((select) => select.value);
+
+selectors.forEach((select) => {
+  while (select.firstChild) {
+    select.removeChild(select.firstChild);
   }
 });
+  
+for (let i = 0; i !== deviceInfos.length; i += 1) {
+  const deviceInfo = deviceInfos[i];
+  const option = document.createElement('option');
 
-*/
+  option.value = deviceInfo.deviceId;
+  if (deviceInfo.kind === 'audioinput') {
+    option.text = deviceInfo.label || `microphone ${audioInputSelect.length + 1}`;
+    audioInputSelect.appendChild(option);
+  }
+  else if (deviceInfo.kind === 'audiooutput') {
+    option.text = deviceInfo.label || `speaker ${audioOutputSelect.length + 1}`;
+    audioOutputSelect.appendChild(option);
+  }
+  else if (deviceInfo.kind === 'videoinput') {
+    option.text = deviceInfo.label || `camera ${videoSelect.length + 1}`;
+    videoSelect.appendChild(option);
+  }
+  else {
+    console.log('Some other kind of source/device: ', deviceInfo);
+  }
+}
+selectors.forEach((select, selectorIndex) => {
+  if (Array.prototype.slice.call(select.childNodes).some((n) => n.value === values[selectorIndex])) {
+    select.value = values[selectorIndex];
+  }
+})
+}).catch(e => console.err(e));
+
+// attaching before the request
+
+audio.deviceId = audioInputSelect.value;
+video.deviceId = videoSelect.value;
+
+//-----
 
 let m;
 
@@ -37,8 +93,6 @@ webex.once("ready", () => {
           alert(err);
           throw err;
         });
-        
-        
       }
     })
     .catch(e => {
@@ -47,13 +101,6 @@ webex.once("ready", () => {
     });
 });
 
-/*
-webex.meetings.register().catch(err => {
-  console.error(err);
-  alert(err);
-  throw err;
-});
-*/
 
 function bindMeetingEvents(meeting) {
   
