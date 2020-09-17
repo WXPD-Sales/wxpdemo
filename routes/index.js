d44a0b315632c188734284eb55977840f0d35270
Glitch (hello-express) <none>
2020-09-17 16:10:10 +0000

    🛎🎽 Checkpoint ./routes/index.js:1701921/123

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Auth0 Webapp sample Nodejs' });
+  //res.render('index', { title: 'Auth0 Webapp sample Nodejs' });
+  res.redirect('https://webex-vaporware.glitch.me/login');
 });
 
 module.exports = router;
